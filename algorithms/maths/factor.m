181db14b24dc3f289d72acaf9f71aeda3662a3d8
Mohsen Asi <mohsen.asi87@gmail.com>
2019-10-10 23:51:56 +0200

    adding algorithm of factorial to math

---
+++ algorithms/maths/factor.m
@@ -0,0 +1,17 @@
+%% Factorial
+function [f] = factor(n)
+% calculate the factorial of a positive integer n
+% factorial(n)can be used directly as a default function of Matlab
+
+integerTest= ~mod(n,1); %it returns 0 if value is not an integer.
+if integerTest== 0 || n < 0;   % checking n to be positive & integer 
+    disp('Error! your number muss be positive and integer');
+else
+    f = 1;
+    for i = 1:n
+        f = f*i;
+    end
+    disp(['factorial of ',num2str(n),' is: ',num2str(f)]);
+end
+end
+
