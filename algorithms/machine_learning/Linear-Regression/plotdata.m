4b5a7c326c2f7cd8b97a25e46529124d936c76b0
Anesh Kaul <aneshkaul@gmail.com>
2020-05-27 16:48:28 +0530

    Added Linear Regression

---
+++ algorithms/machine_learning/Linear-Regression/plotdata.m
@@ -0,0 +1,11 @@
+function plotdata(x, Y)
+
+figure;
+
+plot(x, Y, 'rx', 'MarkerSize', 10); % rx means red coloured x.
+
+ylabel('Profit in $10,000s');
+
+xlabel('Population of city in 10,000s');
+
+end
