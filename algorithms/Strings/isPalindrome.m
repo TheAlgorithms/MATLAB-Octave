97684b8d4002d17a18feec81132ca3064c775940
kNOzone <72206118+kNOzone@users.noreply.github.com>
2020-10-04 12:26:41 +0530

    Update isPalindrome.m

--- algorithms/Strings/isPalindrome.m
+++ algorithms/Strings/isPalindrome.m
@@ -3,16 +3,14 @@ function y = isPalindrome(inputWord)
     
     % Gets the length of the input word.
     inputLength = length(inputWord);
-    
+
     % Number of pairs to compare.
     lastIndex = floor( inputLength / 2);
-    
-    % for every pair of letters in the word check if they match.
+    y=1;
+    % for every pair of letters in the word check if they match
     for i = 1 : lastIndex
         if inputWord(i) ~= inputWord(end + (1 - i))
             y = 0;
-        else
-            y = 1;
         end
     end
     
