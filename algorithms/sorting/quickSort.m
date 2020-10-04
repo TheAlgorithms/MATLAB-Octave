66862502b4df52a236ffe2b93f36ef65bdc5bbf2
puneet-pr-arya <71585635+puneet-pr-arya@users.noreply.github.com>
2020-10-04 11:51:49 +0530

    Create quickSort.m

---
+++ algorithms/sorting/quickSort.m
@@ -0,0 +1,29 @@
+function sortedArray = quickSort(array)
+ 
+    if numel(array) <= 1 %If the array has 1 element then it can't be sorted       
+        sortedArray = array;
+        return
+    end
+ 
+    pivot = array(end);
+    array(end) = [];
+ 
+    %Create two new arrays which contain the elements that are less than or
+    %equal to the pivot called "less" and greater than the pivot called
+    %"greater"
+    less = array( array <= pivot );
+    greater = array( array > pivot );
+ 
+    %The sorted array is the concatenation of the sorted "less" array, the
+    %pivot and the sorted "greater" array in that order
+    sortedArray = [quickSort(less) pivot quickSort(greater)];
+ 
+end
+
+
+%Remark= enter values in the run portion( Run: type code to run)
+%   in the asked format..
+
+ %     quickSort([a,b,c,d,e..])
+
+% a,b,c,d.... can be any interger values
