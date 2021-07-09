0de1181defcf3270212b389c5b789ddfe7f55380
harsh-4 <harshit.bias@gmail.com>
2021-07-09 23:40:28 +0530

    Add jump search algorithm

---
+++ algorithms/Searching/jump_search.m
@@ -0,0 +1,46 @@
+function found_at = jump_search(arr, value)
+  % Contributed by   - Harshit Pant, harshitpant83@gmail.com
+  
+  % arr       - The input array, should be sorted in ascending order.
+  %             'arr' can contain -ve numbers as well as non-integers.
+  % value     - The value to be searched in the array.
+  % found_at  - The index at which 'value' is found in 'arr'.
+  %             -1 is returned if 'value' is not found in 'arr'.
+
+  n = length(arr);
+
+  % 'm' holds the block size
+  m = sqrt(n); 
+  m = round(m); 
+  low = 1;
+  high = 1 + m;
+
+  found_at = -1; 
+
+  while arr(min(high, n)) < value
+      low = high;
+      high = high + m;
+      if low >= n
+          return;
+      endif;
+  endwhile;
+
+  while arr(low) < value
+      low = low + 1;
+      if low > min(high, n)
+          return;
+      endif;
+  endwhile;
+
+  if arr(low) == value
+      found_at = low;
+  endif;
+
+endfunction;
+
+
+% TEST: 
+% Save this file to your local computer, then use the command:
+% jump_search([-11.1, -3.3, -1.3, 0.1, 1.5, 3.5, 3.9, 5.5, 7.5, 9.6, 13.7, 21.3, 35.9], 7.5)
+% This should return, ans = 9.
+% TODO - Transfer this test when automated testing functionality is added to this repository.
