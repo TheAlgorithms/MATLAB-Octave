6147fee674cba67ef8c5296662185c60046e2d3c
MaximSmolskiy <mithridatus@mail.ru>
2021-09-04 19:09:20 +0300

    Fix highest common factor

--- algorithms/maths/highest_common_factor.m
+++ algorithms/maths/highest_common_factor.m
@@ -1,16 +1,16 @@
-ffunction HCF = mini (x, y)
+function HCF = highest_common_factor (x, y)
 %This function searches for highest common factor
 %It first checks if any of the numbers equal to zero, if so the non zero number is the HCF
 %If the numbers are different, you minus the bigger number by the smaller one and this is done by recursion. 
   if x==0
     HCF = y;
   elseif y==0
-    HCF == x;
+    HCF = x;
   elseif (y == x)
     HCF = x;
   elseif (y>x)
-    HCF = mini(y-x,x);
+    HCF = highest_common_factor(y-x,x);
   else 
-    HCF = mini(x-y,y);
+    HCF = highest_common_factor(x-y,y);
   end
 
